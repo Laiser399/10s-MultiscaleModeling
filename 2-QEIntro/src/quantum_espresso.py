26a035d54e04b2e27e976e940a33ff3400c8ad41
Sergey Semenov <Laiser399@mail.ru>
2022-04-17 00:22:08 +0300

    Add ions, cell blocks. Change electrons block.

--- 2-QEIntro/src/quantum_espresso.py
+++ 2-QEIntro/src/quantum_espresso.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Sequence
+from typing import Sequence, Optional
 
 from pydantic import BaseModel
 
@@ -45,8 +45,25 @@ class SystemBlock(BaseBlock):
 
 
 class ElectronsBlock(BaseBlock):
+    conv_thr: Optional[float]
+
     def create_block(self) -> str:
+        conv_thr_str = f'    conv_thr = {self.conv_thr}\n' if self.conv_thr is not None else ''
+
         return f'&electrons\n' \
+               f'{conv_thr_str}' \
+               f'/\n'
+
+
+class IonsBlock(BaseBlock):
+    def create_block(self) -> str:
+        return f'&ions\n' \
+               f'/\n'
+
+
+class CellBlock(BaseBlock):
+    def create_block(self) -> str:
+        return f'&cell\n' \
                f'/\n'
 
 
@@ -72,9 +89,13 @@ class QEConfiguration(BaseBlock):
     control: ControlBlock
     system: SystemBlock
     electrons: ElectronsBlock
+    ions: Optional[IonsBlock]
+    cell: Optional[CellBlock]
     custom_blocks: Sequence[CustomBlock]
 
     def create_block(self) -> str:
+        ions_str = self.ions.create_block() if self.ions is not None else ''
+        cell_str = self.cell.create_block() if self.cell is not None else ''
         custom_blocks_str = ''.join(map(
             lambda x: x.create_block(),
             self.custom_blocks
@@ -83,4 +104,6 @@ class QEConfiguration(BaseBlock):
         return self.control.create_block() \
                + self.system.create_block() \
                + self.electrons.create_block() \
+               + ions_str \
+               + cell_str \
                + custom_blocks_str

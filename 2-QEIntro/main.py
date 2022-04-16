f65fd4cb304bc914557ecfa37f976ee9bcb7e347
Sergey Semenov <Laiser399@mail.ru>
2022-04-16 20:08:47 +0300

    configs generation

--- 2-QEIntro/main.py
+++ 2-QEIntro/main.py
@@ -30,8 +30,8 @@ class SystemBlock(BaseBlock):
     A: float
     nat: int
     ntyp: int
-    ecutwfc: int
-    ecutrho: int
+    ecutwfc: float
+    ecutrho: float
 
     def create_block(self) -> str:
         return f'&system\n' \
@@ -107,15 +107,15 @@ base_configuration = QEConfiguration(
         CustomBlock(
             block_name='ATOMIC_SPECIES',
             lines=[
-                'Sn 118.71 Sn_pbe_v1.uspp.F.UPF'
+                'Ge 72.63 ge_pbe_v1.4.uspp.F.UPF'
             ]
         ),
         CustomBlock(
             block_name='ATOMIC_POSITIONS',
             options=['crystal'],
             lines=[
-                'Sn 0.0 0.0 0.0',
-                'Sn 0.25 0.25 0.25'
+                'Ge 0.0 0.0 0.0',
+                'Ge 0.25 0.25 0.25'
             ]
         ),
         CustomBlock(
@@ -126,5 +126,81 @@ base_configuration = QEConfiguration(
     ]
 )
 
-with open('./in/test.txt', 'w') as output_file:
-    output_file.write(base_configuration.create_block())
+
+def fix_K_BLOCK(k_block: CustomBlock, k: int) -> CustomBlock:
+    k_block.lines = [f'{k} {k} {k} 0 0 0']
+    return k_block
+
+
+def fix_k_block_within_custom_blocks(custom_blocks: Sequence[CustomBlock], k: int):
+    list(map(
+        lambda x: fix_K_BLOCK(x, k),
+        filter(
+            lambda x: x.block_name == 'K_POINTS',
+            custom_blocks
+        )
+    ))
+
+
+def get_config_input_file_path(config_name: str):
+    return f'./in/{config_name}.txt'
+
+
+def create_start_bash_script(file_path: str, config_names: Sequence[str]):
+    with open(file_path, 'w') as output_file:
+        output_file.write('#!/bin/bash\n\n')
+
+        output_file.write('set -e;\n\n')
+
+        for config_name in config_names:
+            config_input_file_path = get_config_input_file_path(config_name)
+
+            output_file.write(f'mkdir ./out/{config_name};\n')
+            output_file.write(f'pw.x -in {config_input_file_path} > ./out/{config_name}/console_output.txt;\n')
+            output_file.write(f'echo "done {config_name}";\n')
+
+
+def generate_config(configuration: QEConfiguration, config_name, k: int, ecutwfc: float):
+    ecutrho_coefficient = 8
+
+    config_input_file_path = get_config_input_file_path(config_name)
+
+    configuration.control.outdir = f'./out/{config_name}'
+    configuration.system.ecutwfc = ecutwfc
+    configuration.system.ecutrho = ecutrho_coefficient * ecutwfc
+    fix_k_block_within_custom_blocks(configuration.custom_blocks, k)
+
+    with open(config_input_file_path, 'w') as output_file:
+        output_file.write(configuration.create_block())
+
+
+def generate_ecutwfc(k: int):
+    configuration = base_configuration.copy(deep=True)
+    config_names = []
+    for ecutwfc in range(10, 100, 10):
+        config_name = f'ecutwfc_{k}_{ecutwfc}'
+
+        generate_config(configuration, config_name, k, ecutwfc)
+
+        config_names.append(config_name)
+
+    create_start_bash_script('start_ecutwfc.sh', config_names)
+
+
+def generate_k(ecutwfc: float):
+    configuration = base_configuration.copy(deep=True)
+    config_names = []
+    for k in range(4, 16, 2):
+        config_name = f'k_{k}_{ecutwfc}'
+
+        generate_config(configuration, config_name, k, ecutwfc)
+
+        config_names.append(config_name)
+
+    create_start_bash_script('start_k.sh', config_names)
+
+
+# dos2unix ./start_ecutwfc.sh; bash ./start_ecutwfc.sh;
+generate_ecutwfc(8)
+# dos2unix ./start_k.sh; bash ./start_k.sh;
+generate_k(60)
